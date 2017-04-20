a324d3264e2df6c34382e35e784a88f74bee5ad3
mnitchev <marionitchev@gmail.com>
2017-04-20 19:48:57 +0300

    Fixed gradient descent algorithm

--- gradient-descent.js
+++ gradient-descent.js
@@ -1,30 +1,43 @@
+var MAX_ITERATIONS = 100
+
 function f(x) {
-    return Math.pow(x, 2)
+    return Math.pow(x, 4)  - 3 * Math.pow(x, 3) +  2
 }
 function df(x) {
-    return 2 * x
+  return 4 * Math.pow(x, 3)  - 9 * Math.pow(x, 2)
 }
 
-function gradientDescent(f, derivative, step, precision, startingPoint) {
-    var previous = startingPoint, current, iterations = 0
-    while (precision < step) {
+function f1(x){
+    return Math.sin(x) / x
+}
+
+function df1(x){
+    return (x * Math.cos(x) - Math.sin(x)) / Math.pow(x, 2)
+}
+
+function gradientDescent(f, derivative, step, gamma,  precision, startingPoint) {
+    var previous = startingPoint, current
+    var iterations = 0
+    while (precision < step && iterations < MAX_ITERATIONS) {
         iterations++
-        current = previous - step * derivative(previous)
-        step = ((current - previous) * (derivative(current) - derivative(previous)))
-        step /= Math.pow(Math.abs(derivative(current) - derivative(previous)), 2)
+        current = previous - gamma * derivative(previous)
+        gamma = ((current - previous) * (derivative(current) - derivative(previous)))
+        gamma /= Math.pow(Math.abs(derivative(current) - derivative(previous)), 2)
+        console.log("gamma : " + gamma + " current : " + current + " previous :" + previous)
+        step = Math.abs(current - previous)
         previous = current
     }
     return {
-        current,
+        point: current,
+        result: f(current),
         iterations
     }
 }
 
-
 function result() {
     var header = document.getElementById('results')
     var iterations = document.getElementById('iterations')
-    var result = gradientDescent(f, df, 2, 0.0000001, [5.5])
-    header.innerText = result.current
+    var result = gradientDescent(f, df, 2, 0.001,  0.0000001, 6)
+    header.innerText = "Point : " + result.point + " Value : " + result.result 
     iterations.innerText = result.iterations
 }
