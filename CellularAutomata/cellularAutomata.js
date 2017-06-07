705c54335d61e21c1475adea0591c47ef61f4244
bojodimitrov <bojo1195@gmail.com>
2017-06-07 23:25:17 +0300

    Implementation of cellular automata

--- CellularAutomata/cellularAutomata.js
+++ CellularAutomata/cellularAutomata.js
@@ -1,6 +1,6 @@
-const green = 'rgb(0, 255, 0)'
+const green = '#B8FEDD'
 const red = 'rgb(255, 0, 0)'
-const black = 'rgb(0, 0, 0)'
+const black = '#5E0000'
 
 class Person {
     constructor(x, y, state) {
@@ -9,12 +9,20 @@ class Person {
         this.state = state;
     }
 
-    getState(){
+    getState() {
         return this.state;
     }
 
-    setState(state){
+    setState(state) {
         this.state = state;
+    }
+
+    getX() {
+        return this.x;
+    }
+
+    getY() {
+        return this.y;
     }
 }
 
@@ -23,6 +31,7 @@ class Population {
         this.width = width;
         this.height = height;
         this.population = [];
+        this.infected = [];
         for (var i = 0; i < width; i++) {
             this.population.push(new Array());
             for (var j = 0; j < height; j++) {
@@ -32,42 +41,136 @@ class Population {
     }
 
 
-    getPopulation(){
+    getPopulation() {
         return this.population;
     }
 
-    defaultInfect(number){
-        for(var i = 0;  i < number; i++){
-                this.population[this.generateRandomNumber(this.width)][this.generateRandomNumber(this.height)].
-                    setState('i');
+    defaultInfect(number) {
+        this.infected = new Array(number);
+        for (var i = 0; i < number; i++) {
+            var randX = this.generateRandomNumber(this.width)
+            var randY = this.generateRandomNumber(this.height)
+            this.population[randX][randY].setState('i')
+            this.infected[i] = this.population[randX][randY]
         }
     }
 
-    generateRandomNumber(max){
+    isInWorld(x, y) {
+        if (x < 0 || y < 0 || x > this.width - 1 || y > this.height - 1) return false
+        return true
+    }
+
+    getNeighbouringInfectedCells(x, y) {
+        var infNeighbours = 0
+        for (var i = -1; i <= 1; i++) {
+            for (var j = -1; j <= 1; j++) {
+                if (this.isInWorld(x + i, y + j) && (i != 0 || j != 0)) {
+                    if (this.population[x + i][y + j].getState() == 'i') {
+                        infNeighbours++
+                    }
+                }
+            }
+        }
+        return infNeighbours
+    }
+
+    willInfect(probability, surroundingCells) {
+        var rand = Math.random()
+        var modelProbabilityForInfection = 1 - Math.pow((1 - probability), surroundingCells)
+        if (rand < modelProbabilityForInfection) {
+            return true
+        }
+        return false
+    }
+
+    willRecover(probability) {
+        var rand = Math.random()
+        if (rand < probability) {
+            return true
+        }
+        return false
+    }
+
+    getNumberOfSurroundingCells(radius) {
+        return Math.pow((2 * radius + 1), 2) - 1
+    }
+
+    update(probability, radius, probabilityR) {
+        for (var i = 0; i < 150; i++) {
+            for (var j = 0; j < 150; j++) {
+                var neighbours = this.getNeighbouringInfectedCells(i, j)
+                if (this.population[i][j].getState() == 's' && this.willInfect(probability, neighbours)) {
+                    this.population[i][j].setState('i');
+                }
+                if (this.population[i][j].getState() == 'i' && this.willRecover(probabilityR)) {
+                    this.population[i][j].setState('r');
+                }
+            }
+        }
+    }
+
+
+    generateRandomNumber(max) {
         return Math.floor((Math.random() * max));
     }
 }
 
-function getPersonColor(state){
-        if(state == 's'){
-            return green;
-        }
-        if(state == 'i'){
-            return red;
-        }
-    }
 
-function draw() {
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext) {
-        var pop = new Population(150, 150);
-        var ctx = canvas.getContext('2d');
-        pop.defaultInfect(20);
-        for(var i = 0; i < 150; i++){
-            for(var j = 0; j < 150; j++){
-                ctx.fillStyle = getPersonColor(pop.getPopulation()[i][j].getState())
-                ctx.fillRect(i*5, j*5, 5, 5);
-            }
+function getPersonColor(state) {
+    if (state == 's') {
+        return green;
+    }
+    if (state == 'i') {
+        return red;
+    }
+    if (state == 'r') {
+        return black;
+    }
+}
+
+
+
+function drawPopulation() {
+    for (var i = 0; i < 150; i++) {
+        for (var j = 0; j < 150; j++) {
+            canvasControl.fillStyle = getPersonColor(pop.getPopulation()[i][j].getState())
+            canvasControl.beginPath()
+            canvasControl.rect(i * 5, j * 5, 5, 5)
+            canvasControl.fill()
+            canvasControl.closePath()
         }
     }
 }
+
+
+//PROBABILITIES - HAVE FUN WITH THEM:
+var infectionProbability = 0.15
+var removeProbability = 0.12
+
+function loop() {//ДЪРВЕНО
+    setInterval(function () {
+        pop.update(infectionProbability, 1, removeProbability)
+        draw()
+    }, 1000 / 60)
+}
+
+function loopAnim() {
+    pop.update(infectionProbability, 1, removeProbability)
+    draw()
+    window.requestAnimationFrame(loopAnim)
+}
+
+
+function draw() {
+    drawPopulation()
+}
+
+var canvas = document.getElementById('canvas');
+var canvasControl = canvas.getContext('2d');
+var pop = new Population(150, 150);
+pop.defaultInfect(6)
+//loop();
+window.requestAnimationFrame(loopAnim)
+
+
+
